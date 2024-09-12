ca53efb963f898526978128418e5ae58efd93b43
mitchydev <paivamit@grinnell.edu>
2024-09-12 00:23:14 -0500

    started Cipher file

--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -2,12 +2,109 @@ package edu.grinnell.csc207.main;
 
 import java.io.PrintWriter;
 
+import edu.grinnell.csc207.util.CipherUtils;
+
 public class Cipher {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
-    for (int i = 0; i < args.length; i++) {
-      pen.printf("args[%d] = \"%s\"\n", i, args[i]);
+
+    if (args.length != 4){
+      System.err.println("Error: Incorrect number of parameters.");
+      pen.close();
+      return;
     }
+
+    String action = null;
+    String ciphertype = null;
+    String message = null;
+    String key = null;
+    boolean afound = false;
+    boolean cfound = false;
+    for(int i = 0; i < args.length; i++)
+    {
+      if(args[i].equals("-encode"))
+      {
+          action = "encode";
+          afound = true;
+      }
+      else if(args[i].equals( "-decode"))
+      {
+          action = "decode";
+          afound = true;
+      }
+      else if(args[i].equals("-caesar"))
+      {
+          ciphertype = "caesar";
+          cfound = true;
+      }
+      else if(args[i].equals("-vigenere"))
+      {
+          ciphertype = "vigenere";
+          cfound = true;
+      }
+      else if (message == null) 
+      {
+        message = args[i];
+      } 
+      else if (key == null) 
+      {
+        key = args[i];
+      }
+      else{
+        System.err.println("Error: Incorrect number of parameters.");
+        pen.close();
+        return;
+      }
+    }
+
+    if (!afound|| !cfound || message == null || key == null){
+      System.err.println("Missing parameters");
+    }
+  
+    if (ciphertype.equals("caesar"))
+    {
+        if (action.equals("encode"))
+        {
+          for (char ch = 'a'; ch <= 'z'; ch++) 
+          {
+            pen.printf("n = %c: %s\n", ch, CipherUtils.caesarEncrypt(message, key.charAt(0)));
+          }
+        }
+        if (action.equals("decode"))
+        {
+        for (char ch = 'a'; ch <= 'z'; ch++) 
+        {
+          pen.printf("n = %c: %s\n", ch, CipherUtils.caesarDecrypt(message, key.charAt(0)));
+        }
+        }
+    }
+
+    if (ciphertype.equals("vigenere"))
+    {
+        if (action.equals("encode"))
+      {
+        for (char ch = 'a'; ch <= 'z'; ch++) 
+        {
+          pen.printf("n = %c: %s\n", ch, CipherUtils.vigenereEncrypt(message, key));
+        }
+      }
+      if (action.equals("decode"))
+      {
+      for (char ch = 'a'; ch <= 'z'; ch++) 
+      {
+        pen.printf("n = %c: %s\n", ch, CipherUtils.vigenereDecrypt(message, key));
+      }
+      }
+  }
+    else 
+    {
+      System.err.println("Error: Incorrect number of parameters.");
+    }
+
     pen.close();
   }
+
+
 }
+
+  
