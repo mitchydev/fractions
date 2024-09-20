5c62dcea78bc7100c450219191857b10363f0995
mitchydev <paivamit@grinnell.edu>
2024-09-19 22:53:47 -0500

    Finished Util Files

--- src/main/java/edu/grinnell/csc207/util/BFRegisterSet.java
+++ src/main/java/edu/grinnell/csc207/util/BFRegisterSet.java
@@ -1,17 +1,60 @@
 package edu.grinnell.csc207.util;
 
 /**
- * Words words words words
+ * A set of registers corresponding to the letters 'a' through 'z'. 
  */
 public class BFRegisterSet {
 
-  public void store(char register, BigFraction val) {
+  private static final int LOWERCASE_A = 97;
+  private static final int REGISTERSIZE = 26;
+  
+  // +--------+-------------------------------------------------------
+  // | Fields |
+  // +--------+
 
+  private final BigFraction[] stores;
+
+  // +--------------+-------------------------------------------------
+  // | Constructors |
+  // +--------------+
+
+  public BFRegisterSet() {
+    this.stores = new BigFraction[REGISTERSIZE];
   }
 
-  // public BigFraction get(char register){
-  //   
+  // +---------+------------------------------------------------------
+  // | Methods |
+  // +---------+
+  /**
+  * Converts a letter to an integer.
+  *
+  * @param letter The letter to convert
+  * @return The integer value of the letter.
+  */
+  private static int letter2int(char letter) {
+    int integer = (int) letter;
+    int newletter = integer - LOWERCASE_A;
+    return newletter;
+  } // class letter2int (char letter)
+
+  /**
+  * Stores a BigFraction for an integer.
+  *
+  * @param letter The letter to convert
+  * @return void
+  */
+  public void store(char register, BigFraction val) {
+    int i = letter2int(register);
+    stores[i] = val;
+  } // store(char register, BigFraciton val)
+
+  public BigFraction get(char register){
+    int i = letter2int(register);
+    if (stores[i] == null) {
+      return new BigFraction(0,1);
+    }
+    return stores[i];
+    }
     
-  // }
-  
 }
+  
